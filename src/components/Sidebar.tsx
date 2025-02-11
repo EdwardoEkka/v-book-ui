e67baeded1a659a92630b27c637582e19113a859
EduardoEkka <edwardekka17@gmail.com>
2025-02-11 15:15:13 +0530

    chore: Login

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Drawer, Box, Typography, Avatar, Stack, Paper } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import {
@@ -10,15 +10,28 @@ import {
   Help,
   Home,
 } from "@mui/icons-material";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { clearUserDetails, setIsAuthenticated } from "../store/auth";
-import { useSelector } from "react-redux";
 import { RootState } from "../store/store";
 
 const Sidebar = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
+  const isAuthenticated = useSelector((state: RootState) => state.user.isAuthenticated);
   const user = useSelector((state: RootState) => state.user.user);
+
+  // Monitor auth state and navigate to /signin when it becomes false
+  useEffect(() => {
+    if (!isAuthenticated) {
+      navigate("/signin", { replace: true });
+    }
+  }, [isAuthenticated, navigate]);
+
+  const handleLogout = () => {
+    dispatch(setIsAuthenticated(false)); // Update Redux
+    dispatch(clearUserDetails());
+    localStorage.removeItem("token");
+  };
 
   return (
     <Drawer
@@ -41,14 +54,13 @@ const Sidebar = () => {
       <Paper
         sx={{
           display: "flex",
-          flexDirection:"column",
+          flexDirection: "column",
           p: "16px",
           alignItems: "center",
           justifyContent: "center",
           position: "relative",
-          boxShadow:3
+          boxShadow: 3,
         }}
-
       >
         <Avatar
           sx={{
@@ -57,27 +69,25 @@ const Sidebar = () => {
             border: "3px solid #3E8E41",
             my: "16px",
           }}
-        ></Avatar>
-        <Typography variant="h6" sx={{}}>
-          {user.name}
-        </Typography>
+        />
+        <Typography variant="h6">{user.name}</Typography>
       </Paper>
       <Stack direction="column" sx={{ px: "40px", py: "32px", gap: "16px" }}>
-        <Stack direction="row" gap={1}>
+        <Stack direction="row" gap={1} onClick={() => navigate("/")} sx={{ cursor: "pointer" }}>
           <Home />
-          <Typography variant="body1" onClick={()=>navigate("/")} sx={{cursor:"pointer"}}>Home</Typography>
+          <Typography variant="body1">Home</Typography>
         </Stack>
-        <Stack direction="row" gap={1} onClick={()=>navigate("/dash-board")} sx={{cursor:"pointer"}}>
+        <Stack direction="row" gap={1} onClick={() => navigate("/dash-board")} sx={{ cursor: "pointer" }}>
           <Dashboard />
           <Typography variant="body1">Dashboard</Typography>
         </Stack>
-        <Stack direction="row" gap={1} onClick={()=>navigate("/profile")} sx={{cursor:"pointer"}}>
+        <Stack direction="row" gap={1} onClick={() => navigate("/profile")} sx={{ cursor: "pointer" }}>
           <Person />
           <Typography variant="body1">Profile</Typography>
         </Stack>
-        <Stack direction="row" gap={1}>
+        <Stack direction="row" gap={1} onClick={() => navigate("/accessibilty")} sx={{ cursor: "pointer" }}>
           <Accessibility />
-          <Typography variant="body1" onClick={()=>navigate("/accessibilty")} sx={{cursor:"pointer"}}>Accessibility</Typography>
+          <Typography variant="body1">Accessibility</Typography>
         </Stack>
       </Stack>
       <Stack sx={{ padding: 2, gap: "16px" }}>
@@ -88,14 +98,12 @@ const Sidebar = () => {
             p: 1,
             justifyContent: "center",
             boxShadow: 3,
-            cursor: "pointer"
+            cursor: "pointer",
           }}
-          onClick={()=>{navigate("/")}}
+          onClick={() => navigate("/")}
         >
-          <Add sx={{ }} />
-          <Typography variant="body1" sx={{ }}>
-            Create new Folder
-          </Typography>
+          <Add />
+          <Typography variant="body1">Create new Folder</Typography>
         </Paper>
         <Paper
           sx={{
@@ -104,14 +112,12 @@ const Sidebar = () => {
             p: 1,
             justifyContent: "center",
             boxShadow: 3,
-            cursor: "pointer"
+            cursor: "pointer",
           }}
-          onClick={()=>{navigate("/")}}
+          onClick={() => navigate("/")}
         >
-          <Add sx={{ }} />
-          <Typography variant="body1" sx={{ }}>
-            Create new File
-          </Typography>
+          <Add />
+          <Typography variant="body1">Create new File</Typography>
         </Paper>
       </Stack>
       <Stack direction="column" sx={{ px: "40px", py: "32px", gap: "16px" }}>
@@ -119,16 +125,7 @@ const Sidebar = () => {
           <Help />
           <Typography variant="body1">Help</Typography>
         </Stack>
-        <Stack
-          direction="row"
-          gap={1}
-          sx={{ cursor: "pointer" }}
-          onClick={() => {
-            setIsAuthenticated(false);
-            dispatch(clearUserDetails());
-            localStorage.removeItem("token");
-          }}
-        >
+        <Stack direction="row" gap={1} sx={{ cursor: "pointer" }} onClick={handleLogout}>
           <Logout />
           <Typography variant="body1">Logout</Typography>
         </Stack>
